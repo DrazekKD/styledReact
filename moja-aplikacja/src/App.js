85722a1985199c4d632a27814a6cf2a0b85de145
Kamil Drazkiewicz <k.drazkiewicz@getprintbox.com>
2020-02-26 11:19:46 +0100

    add styled-component

--- moja-aplikacja/src/App.js
+++ moja-aplikacja/src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import styled from 'styled-components';
 import './App.css';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title>HELOO</Title>
     </div>
   );
 }
