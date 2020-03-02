5e4fc07a44bf92fa76b275a8fc965d004d471827
Kamil Drążkiewicz <k.drazkiewicz@getprintbox.com>
2020-03-02 21:38:02 +0100

    add search input and login input component

---
+++ moja-aplikacja/src/components/atoms/Input/Input.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Input from './Input';
+
+export default {
+  title: 'Input',
+};
+
+export const InputBar = () => <Input placeholder="Login" />;
+export const InputSearchBar = () => <Input placeholder="Login" search />;
